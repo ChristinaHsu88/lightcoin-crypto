4e0046ce9ea324885ab981be3e7ce88f96aab119
Christina <christina.hsu.ca@gmail.com>
2019-02-06 18:37:34 +0000

    set up account functions with deposit and withdrawal

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-let balance = 500.00;
 
-class Withdrawal {
+//account class
+class Account {
+  constructor(username, balance) {
+    this.username = username;
+    this.balance = balance;
+  }
+}
 
-  constructor(amount) {
+// Transaction super class that takes care of the common theme bt withdrawal and deposit class
+class Transaction {
+  constructor(account, amount) {
+    this.account = account;
     this.amount = amount;
+  }
+}
+
+// withdrawal class
+class Withdrawal extends Transaction {
+  
+  get value(value) {
+    return this.amount;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
+  }
+}
+
+//deposit class
+class Deposit extends Transaction {
+
+  get value(value) {
+    return this.amount;
   }
 
+  commit() {
+    this.account.balance += this.amount;
+  }
 }
 
 
@@ -17,13 +44,29 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account('snow-patrol', 500);
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(myAccount, 50.25)
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Transaction1: ', t1);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+// t2 = new Deposit(myAccount, 34)
+// t2.commit();
+// console.log('Transaction2:', t2);
+// t1 = new Withdrawal(50.25);
+// t1.commit();
+// console.log('Transaction 1:', t1);
 
-console.log('Balance:', balance);
+// t2 = new Withdrawal(9.99);
+// t2.commit();
+// console.log('Transaction 2:', t2);
+
+// t3 = new Deposit(20.00);
+// t3.commit();
+// console.log('Transaction 3: ', t3);
+
+// console.log('Balance:', balance);
+
+
+
+
